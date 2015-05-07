86a6346519def2fe906426e6dd2712b5fd5b0289
ronnie nguyen <itsjustronnie@yahoo.com>
2015-05-07 15:24:52 -0700

    updated font and animations until 'i thought bother were porkchops'

--- js/tween.js
+++ js/tween.js
@@ -11,13 +11,20 @@ var tween1 = TweenMax.from('#when-i-was', 2, {
 // create a scene
 new ScrollMagic.Scene({
 		triggerElement: '#when-i-was',
-		offset: 200, /* offset the trigger 150px below #scene's top */
-		duration: 200
-	})
-	.setTween(tween1)
-    .setPin('#when-i-was') // pins the element for the the scene's duration
-    .addTo(controller) // assign the scene to the controller
-    .addIndicators();
+		offset: 600, /* offset the trigger 150px below #scene's top */
+		duration: 400
+})
+.setTween(tween1)
+.addTo(controller) // assign the scene to the controller
+
+new ScrollMagic.Scene({
+	triggerElement: '#when-i-was',
+	offset: 200,
+	duration: 1500
+})
+.setPin('#when-i-was') // pins the element for the the scene's duration
+.addTo(controller) // assign the scene to the controller
+///////// When i was a kid end /////////////
 
 
 ///////// I used to think that ////////////
@@ -28,19 +35,29 @@ var tween2 = TweenMax.from('#think-that', 1, {
 new ScrollMagic.Scene({
 		triggerElement: '#think-that',
 		offset: 250, /* offset the trigger 150px below #scene's top */
-		duration: 650
-	})
-	.setTween(tween2)
-    .setPin('#think-that') // pins the element for the the scene's duration
-    .addTo(controller) // assign the scene to the controller
-    .addIndicators();
+		duration: 400
+})
+.setTween(tween2)
+.addTo(controller) // assign the scene to the controller
 
+new ScrollMagic.Scene({
+	triggerElement: '#think-that',
+	offset: 200,
+	duration: 1500
+})
+.setPin('#think-that') // pins the element for the the scene's duration
+.addTo(controller) // assign the scene to the controller
+
+///////// I used to think that end ////////////
+
+
+//////// porkchops and karatechops /////////
 var slides = document.querySelectorAll("section.panel");
 for (var i = 0; i<slides.length; i++){
 
 	new ScrollMagic.Scene({
 		triggerElement:slides[i],
-		offset: 400
+		offset: 350
 	})
 	.setPin(slides[i])
 	.addIndicators()
@@ -50,6 +67,7 @@ for (var i = 0; i<slides.length; i++){
 
 });
 
+//////// porkchops and karatechops end /////////
 
 
 
