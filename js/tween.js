9c9d558d13f9082ec7b03aeaa4884726e5f8e884
ronnie nguyen <itsjustronnie@yahoo.com>
2015-06-01 23:29:55 -0700

    I hate porkchops

--- js/tween.js
+++ js/tween.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+    var offset = 850;
+    var duration = 300;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('#to-top').fadeIn(duration);
+        } else {
+            jQuery('#to-top').fadeOut(duration);
+        }
+    });
+
+	jQuery('#to-top').click(function(event){
+		event.preventDefault();
+		jQuery('html, body').animate({scrollTop: 0}, duration);
+		return false;
+	})
 		// init controller
 	var controller = new ScrollMagic.Controller();
 
@@ -57,7 +72,7 @@ $(document).ready(function() {
 
 		new ScrollMagic.Scene({
 			triggerElement:slides[i],
-			offset: 350
+			offset: 450
 		})
 		.setPin(slides[i])
 		.addTo(controller);
@@ -150,7 +165,7 @@ $(document).ready(function() {
 
 	new ScrollMagic.Scene({
 		triggerElement: '#not-tell-grandma',
-		offset: 300,
+		offset: 420,
 		duration: 3000
 	})
 	.setPin('#not-tell-grandma') 
@@ -159,7 +174,7 @@ $(document).ready(function() {
 	new ScrollMagic.Scene({
 		triggerElement: "#scared-shed-yell",
 		duration: 3000,
-		offset: 300
+		offset: 420
 	})
 	.setPin('#scared-shed-yell')
 	.addTo(controller)
@@ -199,7 +214,114 @@ $(document).ready(function() {
 	.setTween(wipeAnimation)
 	.addTo(controller);
 
+	new ScrollMagic.Scene({
+		triggerElement: "#lie",
+		duration: 3000,
+		offset: 420
+	})
+	.setPin('#lie')
+	.addTo(controller)
+
+	var tweenAsFarAs = TweenMax.from('#as-far-as', 1, {
+		opacity: 0
+	});
+
+	new ScrollMagic.Scene({
+			triggerElement: '#as-far-as',
+			offset: 250, /* offset the trigger 150px below #scene's top */
+			duration: 400
+	})
+	.setTween(tweenAsFarAs)
+	.addTo(controller) // assign the scene to the controller
+
+	new ScrollMagic.Scene({
+		triggerElement: '#as-far-as',
+		offset: 200,
+		duration: 2000
+	})
+	.setPin('#as-far-as') // pins the element for the the scene's duration
+	.addTo(controller) // assign the scene to the controller
+
+	new ScrollMagic.Scene({
+		triggerElement: "#life-was-good",
+		duration: 3000,
+		offset: 420
+	})
+	.setPin('#life-was-good')
+	.addTo(controller)
+
+	new ScrollMagic.Scene({
+		triggerElement: "#whenever-im-sad",
+		duration: 3000,
+		offset: 420
+	})
+	.setPin('#whenever-im-sad')
+	.addTo(controller)
+
+	new ScrollMagic.Scene({
+		triggerElement: "#gives-chops",
+		duration: 3000,
+		offset: 420
+	})
+	.setPin('#gives-chops')
+	.addTo(controller)
+
 	////// gym teacher end //////////////////
+
+	///// investigation /////////////////////
+
+	var wipeAnimation2 = new TimelineMax()
+		.fromTo("section.dir-panel.led-to", 1, {y:"-100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.filler", 1, {y:"-100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.removed", 1, {y:"100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.filler", 1, {y:"100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.how", 1, {y:"-100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.filler", 1, {y:"-100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.rumors", 1, {y:"100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.filler", 1, {y:"100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.earned-nickname", 1, {y:"-100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.filler", 1, {y:"-100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.nickname", 1, {y:"100%"}, {y:"0%", ease: Linear.easeNone})
+		.fromTo("section.dir-panel.filler", 1, {y:"100%"}, {y:"0%", ease: Linear.easeNone});
+
+	new ScrollMagic.Scene ({
+		triggerElement: "#investigation",
+		triggerHook: "onLeave",
+		duration: 20000
+	})
+	.setPin("#investigation")
+	.setTween(wipeAnimation2)
+	.addTo(controller);
+
+	///// investigation end /////////////////
+
+	var tweenToThisDay = TweenMax.from('#to-this-day', 2, {
+		opacity: 0
+	});
+	// create a scene
+	new ScrollMagic.Scene({
+			triggerElement: '#to-this-day',
+			offset: 600, /* offset the trigger 150px below #scene's top */
+			duration: 400
+	})
+	.setTween(tweenToThisDay)
+	.addTo(controller) // assign the scene to the controller
+
+	new ScrollMagic.Scene({
+		triggerElement: '#to-this-day',
+		offset: 200,
+		duration: 2000
+	})
+	.setPin('#to-this-day') // pins the element for the the scene's duration
+	.addTo(controller) // assign the scene to the controller
+
+	new ScrollMagic.Scene({
+		triggerElement: '#hate',
+		offset: 200,
+		duration: 2500
+	})
+	.setPin('#hate') // pins the element for the the scene's duration
+	.addTo(controller) // assign the scene to the controller
 
 });
 
